4a118a1b5d49bd67aca9f0a9d241ee7b0f448539
yun <oligami@qq.com>
2025-08-28 18:11:54 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://miyugo.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
---
+++ src/content/posts/my-first-blog.md
@@ -0,0 +1,10 @@
+---
+title: my-first-blog
+published: 2025-08-27
+description: '这是我的第一篇博客'
+image: ''
+tags: [测试博客]
+category: ''
+draft: false 
+lang: ''
+---
