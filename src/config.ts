4abd3033eb7308ea0c9244e8b06b389244c672d9
miyugo <43057931+miyugo@users.noreply.github.com>
2025-09-30 18:45:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Yun",
-	bio: "studying,forget,but never give up!",
+	bio: "study and forget,but never give up!",
 	links: [
 		{
 			name: "Twitter",
